2e3350d7644e756e8e9aa2b85c8a665aedc7aac2
Kirill Krylov <k.krylov@creatio.com>
2024-05-23 23:15:05 +0300

    Update Dockerfile and add new DockerfileTool
    The Dockerfile was updated with additional commands for tool installations and entrypoint modification. A new DockerfileTool was created for building a new type of Docker container. These changes also affect the corresponding build scripts.
    
    Signed-off-by: Kirill Krylov <k.krylov@creatio.com>

---
+++ install/build-docker-tool.ps1
@@ -0,0 +1,5 @@
+﻿# CREATE CONTAINER
+docker build -f DockerfileTool -t atf/clio-tool:latest .;
+
+# RUN CONTAINER
+#docker run -it --rm atf/clio-tool:latest;
--- install/build-docker.ps1
+++ install/build-docker.ps1
@@ -1,2 +1,6 @@
-﻿cd ..\clio\bin\Debug\net6.0;
+﻿# CREATE CONTAINER
+cd ..\clio\bin\Debug\net6.0;
 docker build -f ..\..\..\..\install\DockerfileK -t atf/clio:latest .;
+
+# RUN CONTAINER
+docker run -it --rm atf/clio:latest;
